ae5890321bd250d95a026b0523eb4510741d27e8
jbhuva0103 <126946683+jbhuva0103@users.noreply.github.com>
2023-09-21 14:06:22 +0530

    Add files via upload

---
+++ maxminno/src/hello/First_class.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class First_class {
+public static void main(String args[]) {
+	
+	int a=5;
+	int b=4;
+	if(a<b) {
+		System.out.println(b +"is greater");
+	}
+	else {
+		System.out.println(a+" is greater");
+	}
+}
+
+}
